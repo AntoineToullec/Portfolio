c39ad91be42ef2cf7f81cee6da568b6808477e86
AntoineToullec <148762390+AntoineToullec@users.noreply.github.com>
2024-06-17 22:39:30 +0200

    Add files via upload

---
+++ CtrlBillets.java
@@ -0,0 +1,174 @@
+package vue;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class CtrlBillets {
+
+    @FXML
+    private Label categorieBillet;
+
+    @FXML
+    private Label categorieBillet1;
+
+    @FXML
+    private Label categorieBillet11;
+
+    @FXML
+    private Label categorieBillet111;
+
+    @FXML
+    private Label categorieBillet1111;
+
+    @FXML
+    private Label codeBillet;
+
+    @FXML
+    private Label codeBillet1;
+
+    @FXML
+    private Label codeBillet11;
+
+    @FXML
+    private Label codeBillet111;
+
+    @FXML
+    private Label codeBillet1111;
+
+    @FXML
+    private Label dateBillet;
+
+    @FXML
+    private Label dateBillet1;
+
+    @FXML
+    private Label dateBillet11;
+
+    @FXML
+    private Label dateBillet111;
+
+    @FXML
+    private Label dateBillet1111;
+
+    @FXML
+    private Label emplacementBillet;
+
+    @FXML
+    private Label emplacementBillet1;
+
+    @FXML
+    private Label emplacementBillet11;
+
+    @FXML
+    private Label emplacementBillet111;
+
+    @FXML
+    private Label emplacementBillet1111;
+
+    @FXML
+    private Label heureBillet;
+
+    @FXML
+    private Label heureBillet1;
+
+    @FXML
+    private Label heureBillet11;
+
+    @FXML
+    private Label heureBillet111;
+
+    @FXML
+    private Label heureBillet1111;
+
+    @FXML
+    private Label nomGroupeBillet;
+
+    @FXML
+    private Label nomGroupeBillet1;
+
+    @FXML
+    private Label nomGroupeBillet11;
+
+    @FXML
+    private Label nomGroupeBillet111;
+
+    @FXML
+    private Label nomGroupeBillet1111;
+
+    @FXML
+    private Label ouverturePortesBillet;
+
+    @FXML
+    private Label ouverturePortesBillet1;
+
+    @FXML
+    private Label ouverturePortesBillet11;
+
+    @FXML
+    private Label ouverturePortesBillet111;
+
+    @FXML
+    private Label ouverturePortesBillet1111;
+
+    @FXML
+    private ImageView photoBillet;
+
+    @FXML
+    private ImageView photoBillet1;
+
+    @FXML
+    private ImageView photoBillet11;
+
+    @FXML
+    private ImageView photoBillet111;
+
+    @FXML
+    private ImageView photoBillet1111;
+
+    @FXML
+    private Label placeBillet;
+
+    @FXML
+    private Label placeBillet1;
+
+    @FXML
+    private Label placeBillet11;
+
+    @FXML
+    private Label placeBillet111;
+
+    @FXML
+    private Label placeBillet1111;
+
+    @FXML
+    private Label prixBillet;
+
+    @FXML
+    private Label prixBillet1;
+
+    @FXML
+    private Label prixBillet11;
+
+    @FXML
+    private Label prixBillet111;
+
+    @FXML
+    private Label prixBillet1111;
+
+    @FXML
+    private Label rangBillet;
+
+    @FXML
+    private Label rangBillet1;
+
+    @FXML
+    private Label rangBillet11;
+
+    @FXML
+    private Label rangBillet111;
+
+    @FXML
+    private Label rangBillet1111;
+
+}
---
+++ CtrlInfosResa.java
@@ -0,0 +1,59 @@
+package vue;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class CtrlInfosResa {
+
+    @FXML
+    private Button bnBillets;
+
+    @FXML
+    private Button bnImprimer;
+
+    @FXML
+    private Label dateConfirmation;
+
+    @FXML
+    private Label dateReservation;
+
+    @FXML
+    private Label gammePrix;
+
+    @FXML
+    private Label idClient;
+
+    @FXML
+    private Label modePaiement;
+
+    @FXML
+    private Label nomClient;
+
+    @FXML
+    private Label numFacture;
+
+    @FXML
+    private Label numFauteils;
+
+    @FXML
+    private Label numResa;
+
+    @FXML
+    private Label prenomClient;
+
+    @FXML
+    private Label prix;
+
+    @FXML
+    void clicBillets(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clicImprimer(ActionEvent event) {
+
+    }
+
+}
